8bc46a95413fa1d1423fd90018352b980916104c
Akash1507 <atakashsharma@gmail.com>
2020-01-10 06:02:54 +0530

    time field to datetime field

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Message(models.Model):
+    creator = models.ForeignKey(User,on_delete=models.CASCADE,related_name="creator_messages")
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.creator.username
+
+    def last_10_messages():
+        return Message.objects.order_by('-timestamp').all()[:10]
