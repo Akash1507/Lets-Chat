ddd37bfe262d94cfe202a81f7a12d922577bc894
Akash1507 <atakashsharma@gmail.com>
2020-01-13 23:46:48 +0530

    add utility view for user contact

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from chat.models import Chat,Contact
+from django.contrib.auth import get_user_model
 
-def index(request):
-    return render(request, 'chat/index.html', {})
+User = get_user_model()
+def get_last_10_messages(chatId):
+    chat = get_object_or_404(Chat,id=chatId)
+    return chat.messages.order_by('-timestamp').all()[:10]
 
-def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name_json': str(room_name),
-        'username': request.user.username
-    })
+def get_user_contact(username):
+    user = get_object_or_404(User,username=username)
+    return get_object_or_404(Contact,user=user)
+
+def get_current_chat(chatId):
+    return get_object_or_404(Chat,id=chatId)
