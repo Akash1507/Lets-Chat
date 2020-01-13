2b9e37f3763433e564cca655faa6886f69a2667a
Akash1507 <atakashsharma@gmail.com>
2020-01-13 23:42:00 +0530

    register message,chat,contact model

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, Chat, Contact
 # Register your models here
 
 
+admin.site.register(Chat)
 admin.site.register(Message)
+admin.site.register(Contact)
